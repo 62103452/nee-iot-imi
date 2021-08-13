6925cbbfff8bc539293a854f97aa9b3cb6b778ff
62103452 <71056583+62103452@users.noreply.github.com>
2021-08-13 15:17:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   
 <br><br><iframe  src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="400" height="300" frameborder="3"></br></br> </iframe>
 <br><br><iframe  src="https://thingspeak.com/channels/1458744/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="400" height="300" frameborder="3"></br></br> </iframe>
-<br><br><iframe  src="https://thingspeak.com/channels/1458744/maps/channel_show"width="400" height="300" frameborder="3"></br></br> </iframe>
+<iframe  src="https://thingspeak.com/channels/1458744/maps/channel_show"width="400" height="300" frameborder="10"> </iframe>
 
 
     
