7038c9299cbbc0b61b7286aac98487d686357665
62103452 <71056583+62103452@users.noreply.github.com>
2021-08-13 15:04:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <h1> Dararat Khanngang 62103452
 
   
-<br><br><iframe src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15,
-  https://thingspeak.com/channels/1458744/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="500" height="300"></br></br>
-  
+<br><br><iframe src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="500" height="300"></br></br>
+<a href="https://thingspeak.com/channels/1458744/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></a>  
  
 
 
