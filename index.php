9538d59169ae3b0df3dab450ed1a9611a8721346
62103452 <71056583+62103452@users.noreply.github.com>
2021-08-13 14:14:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <h1> Dararat Khanngang
   
-  <iframe src="https://thingspeak.com/channels/1458744/private_show">
+  <iframe src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
