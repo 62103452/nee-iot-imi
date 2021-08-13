416696f4f50e8f31e56a76756914c464d2eeadd7
62103452 <71056583+62103452@users.noreply.github.com>
2021-08-13 14:58:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <h1> Dararat Khanngang 62103452
 
   
-<br><iframe src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="500" height="300"></br>
+<br><br><iframe src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="500" height="300"></br></br>
   
  
 
