c4b00e008d3ad021e96a3798645061be239f0daf
62103452 <71056583+62103452@users.noreply.github.com>
2021-08-18 17:35:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
 
  
 <center><iframe  src="https://thingspeak.com/channels/1458744/maps/channel_show"width="450" height="260" > </center></iframe>  
-<br><iframe  src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
-                width="450" height="300"  title="Humidity" ></br> </iframe>
+<br><br><iframe  src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+                width="450" height="300"  title="Humidity" ></br></br> </iframe>
  <iframe  src="https://thingspeak.com/channels/1458744/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
         width="450" height="300"  title="Temperature"  > </iframe><br>
+ 
      <?php
         $api_url = 'https://api.thingspeak.com/channels/1458744/fields/1.json?results=1';
         $json_data = file_get_contents($api_url);
