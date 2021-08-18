2ef6494ca773597b361fcab68d7475c10892726e
62103452 <71056583+62103452@users.noreply.github.com>
2021-08-18 17:03:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 <h1><center> Dararat Khanngang 62103452</center>
 
 <center><iframe  src="https://thingspeak.com/channels/1458744/maps/channel_show"width="450" height="260" > </center></iframe>  
-<br><br><iframe  src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="300" ></br></br> </iframe>
+<br><br><iframe  src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+                width="450" height="300" title="HUMIDITY" ></br></br> </iframe>
 <iframe  src="https://thingspeak.com/channels/1458744/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="300" > </iframe>
   
      <?php
