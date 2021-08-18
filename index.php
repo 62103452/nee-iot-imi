b8bbccf92d31bc6582f9097af6f9ca0114b77b54
62103452 <71056583+62103452@users.noreply.github.com>
2021-08-18 18:14:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     
   </div>
  </div>
-<center><iframe  src="https://thingspeak.com/channels/1458744/maps/channel_show"width="500" height="300" > </center></iframe>  
+<center><iframe  src="https://thingspeak.com/channels/1458744/maps/channel_show"width="450" height="300" > </center></iframe>  
 <br><br><iframe  src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
                 width="450" height="300"  title="Humidity" ></br></br> </iframe>
  <iframe  src="https://thingspeak.com/channels/1458744/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
