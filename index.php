c3046b69c314525be4bf05524cdbcc7891839d44
62103452 <71056583+62103452@users.noreply.github.com>
2021-08-18 17:12:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 
 
 <h1><center> Dararat Khanngang 62103452</center>
- <center> title="HUMIDITY" </center>
- 
 
+ 
 <center><iframe  src="https://thingspeak.com/channels/1458744/maps/channel_show"width="450" height="260" > </center></iframe>  
 <br><br><iframe  src="https://thingspeak.com/channels/1458744/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
-                width="450" height="300"  ></br></br> </iframe>
-<iframe  src="https://thingspeak.com/channels/1458744/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="300" > </iframe>
+                width="450" height="300"  title="HUMIDITY" ></br></br> </iframe>
+<iframe  src="https://thingspeak.com/channels/1458744/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+        width="450" height="300"  title="Temperature"  > </iframe>
   
      <?php
         $api_url = 'https://api.thingspeak.com/channels/1458744/fields/1.json?results=2';
@@ -26,7 +26,19 @@
         ?>
      </p>
       <br>
-  
+  <?php
+        $api_url = 'https://api.thingspeak.com/channels/1458744/fields/1.json?results=2';
+        $json_data = file_get_contents($api_url);
+        $response_data = json_decode($json_data);
+        $user_data = $response_data->feeds;
+        $user_data = array_slice($user_data, 0);
+        foreach ($user_data as $user) {
+             echo "Temperature: ".$user->field2;
+             echo " C"; 
+         }
+        ?>
+      <br>
+
 
 </body>
 </html>
